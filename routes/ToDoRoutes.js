93852a3e34a8347a752c42ac9b15ddefbbc3d09e
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-06-20 03:50:12 -0700

    shekoofeh-shojai

--- routes/ToDoRoutes.js
+++ routes/ToDoRoutes.js
@@ -1,3 +1,9 @@
 const {Router}=require("express")
 const router=Router();
+const {getToDo,saveToDo,deleteToDo,updateToDo}=require("../controler/ToDoControler")
 router.get("/get-todo",getToDo)
+router.post("/save-todo",saveToDo)
+router.post("/delete-todo",deleteToDo)
+router.post("/update-todo", updateToDo);
+
+module.exports=router

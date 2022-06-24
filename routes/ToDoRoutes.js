5f80ecb6c5886de9f48c77f70235b193405ba2b1
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-06-24 07:58:44 -0700

    shekoofeh-shojai

--- routes/ToDoRoutes.js
+++ routes/ToDoRoutes.js
@@ -3,7 +3,7 @@ const router=Router();
 const {getToDo,saveToDo,deleteToDo,updateToDo}=require("../controler/ToDoControler")
 router.get("/get-todo", getToDo);
 router.post("/save-todo", saveToDo);
-router.post("/delete-todo",deleteToDo)
+router.post("/delete-todo", deleteToDo);
 router.post("/update-todo", updateToDo);
 
 module.exports=router
